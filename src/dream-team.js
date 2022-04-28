074cbd6b51d3c0e6205c4526ce9828f144cf5429
Anneli Savchenko <95057484+Anneli-sf@users.noreply.github.com>
2022-04-28 13:23:03 -0700

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,26 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
+
 function createDreamTeam(/* members */) {
-  let dreamTeam = '';
-   let team = [];
-   let firstLetter ='';
-   
-   if (Array.isArray(members)) {
-     members.sort();
-     if (members.length > 0) {
-       members.forEach(name => {
-         if (typeof name === 'string') {
-           firstLetter = name.trim().toUpperCase().slice(0, 1);
-       
-           team.push(firstLetter);
-         } else return false;
-       }) 
-         console.log(team);
-         dreamTeam = team.sort().join('');
-         console.log(dreamTeam);
-     }
-   } else return false
+  let dreamTeam = "";
+  
+  if (!Array.isArray(members) || !members.length) {
+    return false;
+  }
+ 
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == "string") {
+      dreamTeam += members[i].trim().slice(0, 1);
+      dreamTeam = dreamTeam.toUpperCase();
+      dreamTeam = dreamTeam.split("").sort().join("");
+    }
+  }
+  return dreamTeam;
 }
 
 module.exports = {
